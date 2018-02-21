9e3743602e2811d6fd0e74d2df55884b2b296934
Kelsey Smith <845872897@bun2-206-16.local>
2018-02-21 15:59:46 +1300

    updated files

---
+++ js/kelsey.js
@@ -0,0 +1,43 @@
+var map;
+var infowindow;
+
+function initMap() {
+  var place = {lat: -40.9006, lng: 174.8860};
+
+  map = new google.maps.Map(document.getElementById('map3'), {
+    center: place,
+    zoom: 4
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: place,
+    radius: 80000,
+    type: ['lodging'],
+    name: "YHA"
+  }, callback);
+
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+      console.log(results);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
